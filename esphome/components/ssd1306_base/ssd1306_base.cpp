90260bdc9a5bd4196158807916e7edba6083169b
Chester <chester@easybotics.com>
2022-10-02 21:05:53 -1000

    Update ssd1306_base.cpp

--- esphome/components/ssd1306_base/ssd1306_base.cpp
+++ esphome/components/ssd1306_base/ssd1306_base.cpp
@@ -153,6 +153,7 @@ void SSD1306::display() {
   this->command(SSD1306_COMMAND_COLUMN_ADDRESS);
   switch (this->model_) {
     case SSD1306_MODEL_64_48:
+    case SSD1306_MODEL_72_40:
     case SSD1306_MODEL_64_32:
       this->command(0x20 + this->offset_x_);
       this->command(0x20 + this->offset_x_ + this->get_width_internal() - 1);
